759ace1a2c90a078f3466ef8d1a9d7f267885137
Ignacio Vergara Kausel <ik@odoscope.com>
2017-10-30 16:40:54 +0100

    mods

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,14 +3,14 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Ignacio Vergara Kausel'
-SITENAME = 'IVK Blog'
+SITENAME = 'On data, programming, and technology'
 SITEURL = ''
 
 PATH = 'content'
 
 TIMEZONE = 'Europe/Berlin'
 
-DEFAULT_LANG = 'English'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -19,17 +19,44 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# # Blogroll
+# LINKS = (
+#     ('Pelican', 'http://getpelican.com/'),
+#     ('Python.org', 'http://python.org/'),
+#     ('Jinja2', 'http://jinja.pocoo.org/'),
+# )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('twitter', 'http://twitter.com/ivergarakausel'),
+    ('linkedin', 'https://www.linkedin.com/in/ignaciovergara/'),
+    ('github', 'http://github.com/ivergara'),
+    ('stackoverflow',
+     'https://stackoverflow.com/users/2244081/ignacio-vergara-kausel',
+     'stack-overflow'),
+)
 
 DEFAULT_PAGINATION = 10
 
+THEME = '/home/ignacio/Documents/pelican-themes/pelican-bootstrap3'
+
+BOOTSTRAP_THEME = 'paper'
+
+PLUGIN_PATHS = ['/home/ignacio/Documents/pelican-plugins']
+PLUGINS = ['i18n_subsites', 'ipynb2pelican']
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Options
+
+PYGMENTS_STYLE = 'solarizedlight'
+MARKUP = ('md', 'ipynb')
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_TAGS_ON_SIDEBAR = True
+IGNORE_FILES = ['.ipynb_checkpoints']
+
+DEFAULT_METADATA = {
+    'status': 'draft',
+}
